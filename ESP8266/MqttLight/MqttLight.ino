52b61f6d3ed207e40b0dcc06fa60a86b3ec2b27a
Denislav Trifonov <denislavtrifonov@Denislavs-MacBook-Air.local>
2022-12-29 16:21:27 +0200

    Add initial version of the Arduino file

---
+++ ESP8266/MqttLight/MqttLight.ino
@@ -0,0 +1,18 @@
+/**
+ * @brief  Application for controling WS2812 LEDs using
+ *         MQTT protocol.
+ * @author Denislav Trifonov
+ * @date   29.12.2022
+ */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
